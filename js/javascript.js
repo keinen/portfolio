0fc83da5b941dcafebca30be3e5411790ff915fc
keinen <keinenwyatt@gmail.com>
2018-08-25 10:11:23 +1200

    ready for go live

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,16 @@
 let hamburger = document.querySelector(".hamburger");
 let navUl = document.querySelector(".hide-nav");
+let postButton = document.querySelector('.blog-read-more');
+let postBackground = document.querySelector('.post-page-container-hover')
 
 hamburger.addEventListener("click", function () {
     hamburger.classList.toggle('is-active');
     navUl.classList.toggle('hide-nav');
 });
+
+postButton.addEventListener("mouseover", function() {
+    console.log('mouseover');
+
+});
+
+
